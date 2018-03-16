8589eff4d3e8d89b2a8bbd69c90989ec75412472
zhangap <1130139617@qq.com>
2018-03-16 17:43:17 +0800

    退出登录清除url

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ router.get("/login",function(req,res,next){
  */
 router.get("/logout",function(req,res,next){
   req.session.user = null;
+  req.session.originalUrl = null;
   res.redirect("/login");
 });
 
