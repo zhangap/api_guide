c02b8feed04d853d7249919b29e8633f4ce9d251
zhangap <1130139617@qq.com>
2018-03-20 14:20:44 +0800

    1、增加登录日志 2、修改favicon.ico

--- routes/api.js
+++ routes/api.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var path = require("path");
 var md5 = require("md5");
 var conUtil = require(path.resolve("./public/js/util/conUtil"));
-var Log = require(path.resolve("./public/js/util/log"));
+var appUtil = require(path.resolve("./public/js/util/appUtil"));
 
 var responseData; //返回格式
 router.use(function(req,res,next){
@@ -15,28 +15,13 @@ router.use(function(req,res,next){
 });
 var connection = conUtil.init();
 
-//获取url请求客户端ip
-var get_client_ip = function(req) {
-    debugger;
-    var ip = req.headers['x-forwarded-for'] ||
-        req.ip ||
-        req.connection.remoteAddress ||
-        req.socket.remoteAddress ||
-        req.connection.socket.remoteAddress || '';
-    if(ip.split(',').length>0){
-        ip = ip.split(',')[0]
-    }
-    return ip;
-};
-
-
 /**
  * 登录
  */
 router.post("/login",function(req,res,next){
     var sql = "select * from t_user where username=?";
     var content = req.body;
-    var ip = "";
+    var ip = appUtil.getClientIp(req);
     connection.query(sql,[content.username],function(errors,results){
         if(results.length){
             var user = results[0];
@@ -50,12 +35,12 @@ router.post("/login",function(req,res,next){
                 responseData.message = "密码错误";
                 responseData.statusCode = "10001";
             }
-            Log.loginLog(user.userId,user.username,ip,responseData.msg);
+            appUtil.loginLog(user.userId,user.username,ip,responseData.message);
         }else{
             responseData.status = "error";
             responseData.message = "用户不存在";
             responseData.statusCode = "10002";
-            Log.loginLog("",content.username,ip,responseData.msg);
+            appUtil.loginLog("",content.username,ip,responseData.message);
         }
         res.json(responseData);
     });
