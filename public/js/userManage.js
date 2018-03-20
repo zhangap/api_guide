d7e6b667cb8741f63c223dfcdea130d2ae3b7cb0
yenian <8083@vetech.cn>
2018-03-20 17:50:34 +0800

    增加新样式

--- public/js/userManage.js
+++ public/js/userManage.js
@@ -1,6 +1,34 @@
+var userform=[{
+    username:"wuhan",
+    userrole:"",
+    phone:"",
+    email:"",
+    state:""
+}];
 var app1 = new Vue({
     el:"#content",
     data:{
+        userForm:userform,
         tableData:[]
+    },
+    methods:{
+        renderHeader:function(createElement, _self ) {
+            return createElement(
+                'span',
+                ["操作",
+                    createElement('a', {
+                            'class': 'fa fa-plus-circle ml10 f18',
+                            'href':'javascript:;',
+                            on: { click: function () {
+                                app1.$message({
+                                    message: '这是一条新增操作',
+                                    type: 'success'
+                                });
+                            } }
+                        }
+                    )
+                ]
+            );
+        }
     }
 });
