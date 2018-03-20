434b7594729394b846db243fe2d31b2121896ce1
zhangap <1130139617@qq.com>
2018-03-20 14:04:21 +0800

    增加了log

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var path = require("path");
 var md5 = require("md5");
 var conUtil = require(path.resolve("./public/js/util/conUtil"));
+var Log = require(path.resolve("./public/js/util/log"));
 
 var responseData; //返回格式
 router.use(function(req,res,next){
@@ -14,6 +15,20 @@ router.use(function(req,res,next){
 });
 var connection = conUtil.init();
 
+//获取url请求客户端ip
+var get_client_ip = function(req) {
+    debugger;
+    var ip = req.headers['x-forwarded-for'] ||
+        req.ip ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress || '';
+    if(ip.split(',').length>0){
+        ip = ip.split(',')[0]
+    }
+    return ip;
+};
+
 
 /**
  * 登录
@@ -21,7 +36,7 @@ var connection = conUtil.init();
 router.post("/login",function(req,res,next){
     var sql = "select * from t_user where username=?";
     var content = req.body;
-
+    var ip = "";
     connection.query(sql,[content.username],function(errors,results){
         if(results.length){
             var user = results[0];
@@ -35,14 +50,18 @@ router.post("/login",function(req,res,next){
                 responseData.message = "密码错误";
                 responseData.statusCode = "10001";
             }
+            Log.loginLog(user.userId,user.username,ip,responseData.msg);
         }else{
             responseData.status = "error";
             responseData.message = "用户不存在";
             responseData.statusCode = "10002";
+            Log.loginLog("",content.username,ip,responseData.msg);
         }
+
         res.json(responseData);
     });
 });
+
 
 
 /**
@@ -60,7 +79,12 @@ router.get("/menuList",function(req,res,next){
 
 });
 
-
+/**
+ * 递归处理菜单相关数据
+ * @param resultData
+ * @param results
+ * @param pId
+ */
 function getMenuList(resultData,results,pId){
     for(var i =0,len = results.length;i<len;i++){
         if(results[i].pId == pId){
