28ae41f117c8fe001556f23b60760706c1313118
chuliul <827243631@qq.com>
2018-03-26 20:13:32 +0800

    新增日志 查询页面需要引入的js

---
+++ public/js/logs.js
@@ -0,0 +1,41 @@
+$(function () {
+    var app1 = new Vue({
+        el:"#content",
+        data:{
+            tableData:[
+            ],
+            page:$.extend(true,{},eleUtil.page),
+            admin:{
+                name:""
+            }
+        },
+        created:function(){
+            this.queryLogs();
+        },
+        methods:{
+            queryLogs:function() {
+                var _this=this;
+                $.ajax({
+                    url:"/api/Logs",
+                    type:"get",
+                    data: $.extend(true,{},_this.page,{admin:_this.admin.name}),
+                    success:function (data) {
+                        if(data.status=="success"){
+                            _this.$data.tableData=data.message;
+                            _this.$data.page.currentPage = data.page.currentPage;
+                            _this.$data.page.total = data.page.total;
+                        }
+                    }
+                });
+            },
+            sizeChangeHandler:function (pSize) {
+                this.$data.page.pageSize =pSize;
+                this.queryLogs();
+            },
+            currentChangeHandler:function (cPage) {
+                this.$data.page.currentPage = cPage;
+                this.queryLogs();
+            }
+        }
+    })
+})
