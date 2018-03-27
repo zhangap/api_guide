9b5818b97535cac36bc2b0331ff0306f54bd2264
zhoutz <1075614301@qq.com>
2018-03-27 10:33:40 +0800

    新增和编辑文档类型

--- public/js/documentType.js
+++ public/js/documentType.js
@@ -2,7 +2,15 @@ $(function(){
     var app = new Vue({
         el:'#content',
         data:{
-            documentType:[]
+            typeList:[],
+            dialogVisible:false,
+            winTitle:'新增',
+            typeData:{
+                typeName:'',
+                pId:'',
+                sortNum:'',
+                note:''
+            }
         },
         created:function () {
             this.getDocumentType();
@@ -15,12 +23,55 @@ $(function(){
                     type:'get',
                     success:function(res){
                         if(res.status === 'success'){
-                            _this.$data.documentType = res.data;
+                            _this.$data.typeList = res.data;
                         }else{
                             eleUtil.message(res.message,'error');
                         }
                     }
                 })
+            },
+            addType:function (node,data,handlerType) {
+                this.$data.dialogVisible = true;
+                this.$data.typeData = data;
+                if(handlerType === 'add'){
+                    this.$data.winTitle = '新增文档类型';
+                    this.$data.typeData = {
+                        pId:data.typeId,
+                        preName:data.typeName,
+                        sortNum:'1',
+                        note:''
+                    }
+                }else if(handlerType === 'edit'){
+                    this.$data.winTitle = '编辑文档类型';
+                    this.$data.typeData = data;
+                }
+            },
+            removeType:function (node,data) {
+                console.log('node',node);
+                console.log('data',data)
+            },
+            closeMenuWinHandler:function () {
+                this.$data.dialogVisible = false;
+            },
+            submitMenuHandler:function () {
+                var _this = this;
+                console.log(this.$data.typeData)
+                $.ajax({
+                    url:"/api/saveType",
+                    type:"post",
+                    data:this.$data.typeData,
+                    success:function(data){
+                        if(data.status === "success"){
+                            eleUtil.message(data.message,"success");
+                            _this.$data.dialogVisible = false;
+                            setTimeout(function(){
+                                location.reload();
+                            },500);
+                        }else{
+                            eleUtil.message(data.message,"error");
+                        }
+                    }
+                });
             }
         }
     })
